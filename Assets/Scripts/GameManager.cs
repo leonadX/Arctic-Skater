0ba00cb51eef0c741339e94cb3c905eb045e394f
leonadX <achimugu24@gmail.com>
2022-07-31 20:26:32 +0100

    upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private const int COIN_SCORE_AMOUNT = 5 ;
+
+    public static GameManager Instance { set; get; }
+
+    public bool IsDead { set; get; }
+    private bool isGameStarted = false;
+    private PlayerMotor motor;
+
+    // UI and the UI fields
+    public Animator gameCanvas, menuAnim, diamondAnim;
+    public Text scoreText, coinText, modifierText, hiscoreText;
+    private float score, coinScore, modifierScore;
+    private int lastScore;
+
+    // Death Menu
+    public Animator deathMenuAnim;
+    public Text deadScoreText, deadCoinText;
+
+    private void Awake()
+    {
+
+        Instance = this;
+        modifierScore = 1;
+        // UpdateScores();
+        motor = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>();
+
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+        coinText.text = coinScore.ToString("0");
+        scoreText.text = score.ToString("0");
+
+        hiscoreText.text = PlayerPrefs.GetInt("Hiscore").ToString();
+    }
+
+    public void Update()
+    {
+        if(MobileInput.Instance.Tap && !isGameStarted)
+        {
+            isGameStarted = true;
+            motor.StartRunning();
+            FindObjectOfType<GlacierSpawner>().IsScrolling = true;
+            FindObjectOfType<CameraMotor>().IsMoving = true;
+            gameCanvas.SetTrigger("Show");
+            menuAnim.SetTrigger("Hide");
+        }
+
+        if (isGameStarted && !IsDead)
+        {
+            // Bump up the score
+            score += (Time.deltaTime * modifierScore);
+
+            if (lastScore != score)
+            {
+                lastScore = (int)score;
+                scoreText.text = score.ToString("0");
+            }
+        }
+    }
+
+    public void GetCoin()
+    {
+        diamondAnim.SetTrigger("Collect");
+        coinScore++;
+        score += COIN_SCORE_AMOUNT;
+        coinText.text = coinScore.ToString("0");
+        scoreText.text = score.ToString("0");
+        
+    }
+
+    public void UpdateModifier(float modifierAmount)
+    {
+        modifierScore = 1.0f + modifierAmount;
+        modifierText.text = "x" + modifierScore.ToString("0.0");
+    }
+
+    public void OnPlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void OnDeath()
+    {
+        IsDead = true;
+        FindObjectOfType<GlacierSpawner>().IsScrolling = false;
+        deadScoreText.text = score.ToString("0");
+        deadCoinText.text = coinScore.ToString("0");
+        deathMenuAnim.SetTrigger("Dead");
+        gameCanvas.SetTrigger("Hide");
+
+        // Check if this is a highscore
+        if (score > PlayerPrefs.GetInt("Hiscore"))
+        {
+            float s = score;
+            if (s % 1 == 0)
+                s += 1;
+            PlayerPrefs.SetInt("Hiscore", (int)s);
+        }
+    }
+}
---
+++ Assets/Scripts/GlacierSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlacierSpawner : MonoBehaviour
+{
+    private const float DISTANCE_TO_RESPAWN = 10.0f;
+
+    public float scrollSpeed = -2;
+    public float totalLength;
+    public bool IsScrolling { set; get; }
+
+    private float scrollLastIncrease;
+    private float scrollIncreaseTime = 4f;
+    private float scrollIncreaseAmt = -0.6f;
+    private float scrollLocation;
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (!IsScrolling)
+            return;
+
+        if(Time.time - scrollLastIncrease > scrollIncreaseTime)
+        {
+            scrollLastIncrease = Time.time;
+            scrollSpeed += scrollIncreaseAmt;
+        }
+
+        scrollLocation += scrollSpeed * Time.deltaTime;
+        Vector3 newLocation = (playerTransform.position.z + scrollLocation) * Vector3.forward;
+        transform.position = newLocation;
+
+        if(transform.GetChild(0).transform.position.z < playerTransform.position.z - DISTANCE_TO_RESPAWN)
+        {
+            transform.GetChild(0).localPosition += Vector3.forward * totalLength;
+            transform.GetChild(0).SetSiblingIndex(transform.childCount);
+
+            transform.GetChild(0).localPosition += Vector3.forward * totalLength;
+            transform.GetChild(0).SetSiblingIndex(transform.childCount);
+        }
+    }
+}
